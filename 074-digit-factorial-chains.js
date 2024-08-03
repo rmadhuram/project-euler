f361a8233dd83fc947fd6bf5b7122afa2acdda7b
rmadhuram <rmadhuram@yahoo.com>
2024-08-03 20:00:44 +0530

    problem 74

---
+++ 074-digit-factorial-chains.js
@@ -0,0 +1,62 @@
+let fact = []
+let counts = []
+fact[0] = 1
+
+// precompute factorials.
+for (let i = 1; i < 10; i++) {
+  fact[i] = i * fact[i-1]
+}
+
+// Get next number in chain.
+function calcNext(n) {
+  let sum = 0
+  for (let k of n+'') {
+    sum += fact[+k]
+  }
+  return sum
+}
+
+let nSixty = 0
+let graph = {}
+for (let n = 1; n < 1000000; n++) {
+  if (n % 10000 == 0) {
+    console.log(`calc for ${n}`)
+  }
+  
+  let k = n
+  while (true) {
+    let k1 = calcNext(k)
+    if (graph[k1]) {
+      graph[k] = k1
+      break;
+    } 
+    graph[k] = k1
+    k = k1
+  }
+
+  // walk to find the path length
+  let pathLength = 0
+  let map = {}
+  k = n
+  let str = n
+  while (!map[k]) {
+    //console.log(`k is ${k}`)
+    pathLength++
+    map[k] = 1
+    k = graph[k]
+    if (k) {
+      str += ` -> ${k}`
+    }
+  }
+
+  if (pathLength == 60) {
+    console.log(`   *** Found: ${n}`)
+    nSixty++
+
+  }
+
+  //console.log(str)
+  //console.log(`${n} -- len: ${pathLength}`)
+}
+
+console.log(nSixty)
